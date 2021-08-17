e339f851460b6e5c727cef9dda01a686e6eda5a4
Mali0210 <56685977+sanayut12@users.noreply.github.com>
2021-08-18 01:53:55 +0700

    origin
    first

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+var port = process.env.port || 3000
+app.use(express.json())
+
+/////////////   api service     ////////////
+app.get('/',(req,res)=>{
+    res.send("<h1>scb recieve callback</h1>")
+})
+
+app.get('/:name', (req, res) => {
+    // res.sendFile(__dirname + '/index.html');
+    var {name}= req.params
+    console.log(name)
+    io.emit('test', name);
+    res.send(name)
+});
+
+app.post('/',(req,res)=>{
+    var {
+        name
+    } = req.body
+    console.log(name)
+    res.json({"name is" : name})
+})
+
+app.post('/recieve-endpoint',(req,res)=>{
+    var data = req.body
+    io.emit('scb-callback', JSON.stringify(data));
+    res.json({})
+})
+
+app.post('/recieve-callback',(req,res)=>{
+    var data = req.body
+    io.emit('scb-callback', JSON.stringify(data));
+    res.json({})
+})
+
+///////////     socket      /////////////////
+io.on('connection', (socket) => {
+    socket.on('scb-callback', msg => {
+      console.log(msg)
+      io.emit('scb-callback', msg);
+    });
+
+    socket.on('scb-endpoint', msg => {
+        console.log(msg)
+        io.emit('scb-endpoint', msg);
+      });
+
+    socket.on('test', msg => {
+        console.log(msg)
+        io.emit('test', msg);
+      });
+});
+
+http.listen(port,()=>{
+    console.log(`http://localhost:${port}`)
+})
+
