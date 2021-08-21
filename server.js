5cb73f5845cab74dfe3a2ae659e254f61817551e
Mali0210 <56685977+sanayut12@users.noreply.github.com>
2021-08-21 14:16:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
+const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 var port = process.env.PORT || 3000
 app.use(express.json())
